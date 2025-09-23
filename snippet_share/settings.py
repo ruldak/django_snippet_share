792771f046ca1e5a379b2c0fefa333b7a4aa7290
ruldak <ruldak3306@gmail.com>
2025-09-23 18:40:22 +0700

    add sqlite database for testing

--- snippet_share/settings.py
+++ snippet_share/settings.py
@@ -132,6 +132,14 @@ DATABASES = {
     }
 }
 
+# Switch to SQLite for testing
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
