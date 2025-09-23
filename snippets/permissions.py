9fd5276740f787172f29c32c98b250d5aac21b94
ruldak <ruldak3306@gmail.com>
2025-09-23 18:38:36 +0700

    change the docstring language from Indonesian to English

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -2,13 +2,11 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custom permission untuk hanya mengizinkan pemilik suatu object untuk mengeditnya.
+    Custom permission to only allow owners of an object to edit it.
     """
     
     def has_object_permission(self, request, view, obj):
-        # Read permissions diizinkan untuk request apapun
         if request.method in permissions.SAFE_METHODS:
             return True
         
-        # Write permissions hanya diizinkan untuk pemilik snippet
         return obj.user == request.user
